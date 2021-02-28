e6bfca352ad95f0bb6e360b288f264c45848377d
Nisharu <39903426+Nisharu@users.noreply.github.com>
2021-02-28 21:18:21 +0200

    Added Session, GameManager

--- Fug_intermediate/Fug_intermediate.cpp
+++ Fug_intermediate/Fug_intermediate.cpp
@@ -4,7 +4,9 @@
 #include <map>
 #include <ctime>
 #include <numeric>
-
+#include <chrono>
+#include <iomanip>
+#include <time.h>
 using namespace std;
 
 class Hero
@@ -34,6 +36,7 @@ public:
 		Rank = pRank;
 	}
 };
+
 class PlayerManager
 {
 public:
@@ -79,7 +82,7 @@ public:
 		Player empty_player(0, "This player does not exist", 0);
 		return empty_player;
 	}
-	void DeletePlayer(string pName = "", int pId = 0 )
+	void DeletePlayer(string pName = "", int pId = 0)
 	{
 		this->players;
 		for (int i = 0; i < players.size(); i++)
@@ -90,6 +93,12 @@ public:
 				break;
 			}
 		}
+	}
+	void ChangeRank(int pId, int rank_change = 0)
+	{
+		this->players;
+
+		players[pId - 1].Rank += rank_change;
 	}
 	void AllPlayersInfo()
 	{
@@ -106,7 +115,7 @@ public:
 			{
 				cout << "Name: " << players[i].Name << "\t" << "Id: " << players[i].Id << "\tRank: " << players[i].Rank << endl;
 				break;
-				
+
 			}
 		}
 	}
@@ -115,7 +124,7 @@ class HeroManager
 {
 public:
 	vector<Hero> heroes;
-	void CreateHero(int Id, string Name, int HP , int Damage)
+	void CreateHero(int Id, string Name, int HP, int Damage)
 	{
 		Hero new_hero(Id, Name, HP, Damage);
 		heroes.push_back(new_hero);
@@ -130,7 +139,7 @@ public:
 				return heroes[i];
 			}
 		}
-		Hero empty_hero(0, "This player does not exist", 0,0);
+		Hero empty_hero(0, "This player does not exist", 0, 0);
 		return empty_hero;
 	}
 
@@ -190,63 +199,156 @@ public:
 		Hero hero;
 		PlayerHero() : player(), hero()
 		{
-
 		}
 	};
 
-	vector<PlayerHero> team;
-
+	vector<PlayerHero> players;
 };
 
 class TeamManager
 {
 public:
-	
+
 	vector<Team> teams;
 	void GenerateNewTeams(PlayerManager players, HeroManager heroes)
 	{
 		this->teams;
 		teams.clear();
-		
+
+		int p_iterator;
 		int random_player;
 		vector<int> random_players(players.players.size());
 		iota(random_players.begin(), random_players.end(), 0);
 
+		int h_iterator;
 		int random_hero;
 		vector<int> random_heroes(heroes.heroes.size());
 		iota(random_heroes.begin(), random_heroes.end(), 0);
 
-		for (int t = 0; t<2; t++)
+		for (int t = 0; t < 2; t++)
 		{
 			teams.resize(t + 1);
 			for (int i = 0; i < 5; i++)
 			{
-				random_player = rand() % random_players.size();
-				random_players.erase(random_players.begin() + random_player);
+				p_iterator = rand() % random_players.size();
+				random_player = random_players[p_iterator];
+				random_players.erase(random_players.begin() + p_iterator);
 
-				random_hero = rand() % random_heroes.size();
-				random_heroes.erase(random_heroes.begin() + random_hero);
-				teams[t].team.resize(i+1);
-				teams[t].team[i].player = players.players[random_player];
-				teams[t].team[i].hero = heroes.heroes[random_hero];
+				h_iterator = rand() % random_heroes.size();
+				random_hero = random_heroes[h_iterator];
+				random_heroes.erase(random_heroes.begin() + h_iterator);
+
+				teams[t].players.resize(i + 1);
+				teams[t].players[i].player = players.players[random_player];
+				teams[t].players[i].hero = heroes.heroes[random_hero];
 			}
 		}
-		
 	}
-	void GetTeamInfo(PlayerManager& players, HeroManager& heroes)
+	void GetTeamInfo(PlayerManager players, HeroManager heroes)
 	{
 		for (const auto& elem : teams)
 		{
 			for (int i = 0; i < 5; i++)
 			{
-				players.ShowPlayerInfo(elem.team[i].player.Name);
-				heroes.ShowHeroInfo(elem.team[i].hero.Name);
+				players.ShowPlayerInfo(elem.players[i].player.Name);
+				heroes.ShowHeroInfo(elem.players[i].hero.Name);
 			}
 			cout << "\n\n\n";
 		}
 	}
-
 };
+
+class Session
+{
+public:
+	string start_time;
+	Team team_one;
+	Team team_two;
+	Team winner;
+
+	Session(Team _team_one, Team _team_two)
+		: team_one(_team_one), team_two(_team_two)
+	{
+		//time_t curr_time = time(0);
+		//start_time = ctime(&curr_time);
+		winner = CalculateWinner(_team_one, _team_two);
+	}
+
+	Team CalculateWinner(Team _team_one, Team _team_two)
+	{
+		int team_one_damage = 0, team_two_damage = 0;
+		int team_one_health = 0, team_two_health = 0;
+		for (int i = 0; i < _team_one.players.size(); i++)
+		{
+			team_one_damage = _team_one.players[i].hero.Damage;
+			team_one_health = _team_one.players[i].hero.HP;
+		}
+		for (int i = 0; i < _team_two.players.size(); i++)
+		{
+			team_two_damage = _team_two.players[i].hero.Damage;
+			team_two_health = _team_two.players[i].hero.HP;
+		}
+
+		if (team_one_health - team_two_damage < team_two_health - team_one_damage)
+		{
+			return _team_two;
+		}
+		return _team_one;
+	}
+	void ShowSession()
+	{
+		cout << start_time<< "Winner: " << endl;
+		for (auto &elem : winner.players)
+		{
+			cout << elem.player.Name << "  " << elem.player.Rank << endl;
+		}
+	}
+};
+
+class GameManager
+{
+	vector<Session> GameSessions;
+	PlayerManager players;
+	HeroManager heroes;
+	TeamManager teams;
+public:
+
+	GameManager(PlayerManager _players, HeroManager _heroes)
+		: players(_players), heroes(_heroes)
+	{
+	}
+
+	void PerformGameSession(PlayerManager& players)
+	{
+		teams.GenerateNewTeams(players, heroes);
+
+		for (int i = 0; i < teams.teams.size() - 1; i++)
+		{
+			Session temp(teams.teams[i], teams.teams[i + 1]);
+
+			for (int i = 0; i < temp.team_one.players.size(); i++)
+			{
+				players.ChangeRank(temp.team_one.players[i].player.Id, -25);
+				players.ChangeRank(temp.team_two.players[i].player.Id, -25);
+				players.ChangeRank(temp.winner.players[i].player.Id, 50);
+				temp.winner.players[i].player.Rank += 25;
+			}
+
+			GameSessions.push_back(temp);
+		}
+
+		teams.GetTeamInfo(players, heroes);
+	}
+	void ShowSessions()
+	{
+		for (auto &elem : GameSessions)
+		{
+			elem.ShowSession();
+			cout << endl;
+		}
+	}
+};
+
 void InitializeUnits(PlayerManager& players, HeroManager& heroes)
 {
 	vector<string> player_names = { "Glebelul", "Glebid", "Glebuhnuv", "Gleb Proshlogodniy","Glebilliard", "Glebanka","Glebabka","Glebatya","Gleboomer","Glebaran","Gleboomerang","Gleboohanka","Glebochka","Glebochko","Glebass", "Glebariga" };
@@ -259,7 +361,7 @@ void InitializeUnits(PlayerManager& players, HeroManager& heroes)
 		temp_name = player_names[random];
 		player_names.erase(player_names.begin() + random);
 
-		players.CreatePlayer(i+1, temp_name , 0);
+		players.CreatePlayer(i + 1, temp_name, 0);
 	}
 	for (int i = 0; i < 15; i++)
 	{
@@ -267,31 +369,22 @@ void InitializeUnits(PlayerManager& players, HeroManager& heroes)
 		temp_name = hero_names[random];
 		hero_names.erase(hero_names.begin() + random);
 
-		heroes.CreateHero(i+1, temp_name, rand() % 60 + 10, rand() % 60 + 10);
+		heroes.CreateHero(i + 1, temp_name, rand() % 60 + 10, rand() % 60 + 10);
 	}
-
 }
+
 int main()
 {
-
 	srand(time(NULL));
+
 	PlayerManager players;
 	HeroManager heroes;
-	TeamManager teams;
 	InitializeUnits(players, heroes);
+	GameManager game_manager(players, heroes);
+	game_manager.PerformGameSession(players);
+	game_manager.PerformGameSession(players);
+	game_manager.PerformGameSession(players);
+	game_manager.ShowSessions();
 
-	teams.GenerateNewTeams(players, heroes);
-
-	teams.GetTeamInfo(players, heroes);
-	//players.CreatePlayer(1, "Glebid", 0);
-	//players.CreatePlayer(2, "Andrel", 0);
-	//players.CreatePlayer(3, "Dimkrat", 0);
-	//heroes.CreateHero(1, "Glebelul", 1, 1);
-	
-	//players.AllPlayersInfo();
-	//heroes.AllHeroesInfo();
-	//players.DeletePlayer("" , 1);
-	//players.GetPlayerById(1).hero = heroes.GetHeroById(1);
-	//players.AllPlayersInfo();
-	//players.ShowPlayerInfo("" , 2);
+	return 0;
 }
